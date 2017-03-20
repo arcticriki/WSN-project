a12d10d231051b545e47ecd08d273d10e0d2ae2a
Soldelli92 <mattia.soldan.ms@gmail.com>
2017-03-20 18:07:40 +0100

    Prova

--- Node.py
+++ Node.py
@@ -4,14 +4,22 @@ class Node(object):
         self.ID = ID
         self.X = X
         self.Y = Y
+        self.neighbor_list = []
 
-    def node_write(self, ID, X, Y): #change ID and position coordinates
+    def node_write(self, ID, X, Y, neighbor): #change ID and position coordinates
         self.ID = ID
         self.X = X
         self.Y = Y
+        #self.neighbor_list.append(neighbor)
 
     def spec(self):
         print 'Node ID is %d and its position is (x=%d, y=%d)' % (self.ID, self.X, self.Y)
+        print self.neighbor_list
 
     def get_pos(self):
         return self.X, self.Y
+
+    def neighbor_write(self, neighbor):
+        self.neighbor_list.append(neighbor)
+
+
--- main.py
+++ main.py
@@ -3,21 +3,33 @@ import random as rnd
 import matplotlib.pyplot as plt
 from Node import Node
 
-n = 10
+n = 1000  # number of nodes
 k = 5
-L = 50   # lato del quadrato in metri
+L = 50   # square dimension
 
+# n1 = Node(1,0,0)
+# n2 = Node(2,1,1)
+# n2.neighbor_write(n1)
+# n1.ID=8
+# n2.neighbor_list[0].spec()
 node_list = [Node(_+1, rnd.uniform(0.0, L), rnd.uniform(0.0, L)) for _ in xrange(n)]        # initialization
 
 
+# Find nearest neighbours using euclidean distance
 x = np.zeros(n)
 y = np.zeros(n)
 for i in xrange(n):
+    for j in xrange(n):
+        dist = np.sqrt(np.sum(np.square(np.asarray(node_list[i].get_pos()) - np.asarray(node_list[j].get_pos()))))
+        if dist <= 1 and dist != 0:
+            node_list[i].neighbor_write(node_list[j])
+
+for i in xrange(n):      # printing the results
     node_list[i].spec()
     [x[i], y[i]] = node_list[i].get_pos()
 
 
-plt.title('Graphical representation of sensor positions. ')
+plt.title("Graphical representation of sensors' positions")
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.grid()
