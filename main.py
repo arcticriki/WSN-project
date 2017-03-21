e26495bac67fd78bb98a1b5795cd854ca0fccc5d
Soldelli92 <mattia.soldan.ms@gmail.com>
2017-03-21 11:20:38 +0100

    Prova

--- main.py
+++ main.py
@@ -10,21 +10,37 @@ n = 1000  # number of nodes
 k = 200
 L = 100   # square dimension
 
-node_list = [Storage(_+1, rnd.uniform(0.0, L), rnd.uniform(0.0, L)) for _ in xrange(n)]        # initialization
+positions = np.zeros((n, 2))
+node_list = []
+for i in  xrange(n):
+    x = rnd.uniform(0.0, L)
+    y = rnd.uniform(0.0, L)
+    node_list.append(Storage(i+1, x, y))       # initialization
+    positions[i, :] = [x, y]
+
 sensors_position = rnd.sample(range(0, n), k)
 for i in xrange(len(sensors_position)):
-    node_list[sensors_position[i]] = Sensor(sensors_position[i]+1, rnd.uniform(0.0, L), rnd.uniform(0.0, L))
+    x = rnd.uniform(0.0, L)
+    y = rnd.uniform(0.0, L)
+    node_list[sensors_position[i]] = Sensor(sensors_position[i]+1, x, y)
+    positions[i, :] = [x, y]
+
 
 # Find nearest neighbours using euclidean distance
 x = np.zeros(n)
 y = np.zeros(n)
 u = np.zeros(n)
 v = np.zeros(n)
-for i in xrange(n):
-    for j in xrange(n):
-        dist = np.sqrt(np.sum(np.square(np.asarray(node_list[i].get_pos()) - np.asarray(node_list[j].get_pos()))))
-        if dist <= 1 and dist != 0:
-            node_list[i].neighbor_write(node_list[j])
+diff=np.zeros(2)
+dmax = 1                            # maximum distance for communication
+dmax2 = dmax*dmax                   # square of maximum distance for communication
+for i in xrange(n):                 # cycle on all nodes
+    for j in xrange(n):             # compare each node with all the others
+        diff = [positions[i, 0] - positions[j, 0],positions[i, 1] - positions[j, 1]]
+        dist = diff[0]*diff[0] + diff[1]*diff[1]
+        if dist <= dmax2:
+            if dist != 0:
+                node_list[i].neighbor_write(node_list[j])
 
 for i in xrange(n):      # printing the results
     #node_list[i].spec()
