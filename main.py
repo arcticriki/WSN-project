f865e447886d3592dcfddcf8cd762abc1053c62e
Soldelli92 <mattia.soldan.ms@gmail.com>
2017-03-21 12:45:24 +0100

    Prova

--- main.py
+++ main.py
@@ -1,64 +1,60 @@
-import numpy as np
-import random as rnd
-import matplotlib.pyplot as plt
-import time as time
-from Node import *
+import numpy as np                              # import of package numpy for mathematical tools
+import random as rnd                            # import of package random for homonym tools
+import matplotlib.pyplot as plt                 # import of package matplotlib.pyplot for plottools
+import time as time                             # import of package time for monitoring computational time
+from Node import *                              # * means we import both Storage() and Sensor() classes
 
-t = time.time()
+t = time.time()                                 # initial timestamp
 
-n = 1000  # number of nodes
-k = 200
-L = 100   # square dimension
+# PARAMETER INITIALIZATION SECTION
 
-positions = np.zeros((n, 2))
-node_list = []
-for i in  xrange(n):
-    x = rnd.uniform(0.0, L)
-    y = rnd.uniform(0.0, L)
-    node_list.append(Storage(i+1, x, y))       # initialization
+n = 1000                                        # number of nodes
+k = 200                                         # number of sensors
+L = 100                                         # square dimension
+
+positions = np.zeros((n, 2))                    # matrix containing info on all node positions
+node_list = []                                  # list of references to node objects
+dmax = 1                                        # maximum distance for communication
+dmax2 = dmax * dmax                             # square of maximum distance for communication
+sensors_position = rnd.sample(range(0, n), k)   # generation of random indices for sensors
+
+# NETWORK INITIALIZATION
+
+# Generation of storage nodes
+for i in xrange(n):                             # for on 0 to n indices
+    x = rnd.uniform(0.0, L)                     # generation of random coordinate x
+    y = rnd.uniform(0.0, L)                     # generation of random coordinate y
+    node_list.append(Storage(i + 1, x, y))      # creation of storage node, function Storage()
     positions[i, :] = [x, y]
 
-sensors_position = rnd.sample(range(0, n), k)
-for i in xrange(len(sensors_position)):
-    x = rnd.uniform(0.0, L)
-    y = rnd.uniform(0.0, L)
-    node_list[sensors_position[i]] = Sensor(sensors_position[i]+1, x, y)
-    positions[i, :] = [x, y]
-
+# Generation of sensor nodes
+for i in sensors_position:                      # for on sensors position indices
+    x = rnd.uniform(0.0, L)                     # generation of random coordinate x
+    y = rnd.uniform(0.0, L)                     # generation of random coordinate y
+    node_list[i] = Sensor(i + 1, x, y)          # creation of sensor node, function Sensor(), extend Storage class
+    positions[i, :] = [x, y]                    # support variable for positions info, used for comp. optim. reasons
 
 # Find nearest neighbours using euclidean distance
-x = np.zeros(n)
-y = np.zeros(n)
-u = np.zeros(n)
-v = np.zeros(n)
-diff=np.zeros(2)
-dmax = 1                            # maximum distance for communication
-dmax2 = dmax*dmax                   # square of maximum distance for communication
-for i in xrange(n):                 # cycle on all nodes
-    for j in xrange(n):             # compare each node with all the others
-        diff = [positions[i, 0] - positions[j, 0],positions[i, 1] - positions[j, 1]]
-        dist = diff[0]*diff[0] + diff[1]*diff[1]
-        if dist <= dmax2:
-            if dist != 0:
-                node_list[i].neighbor_write(node_list[j])
+for i in xrange(n):                             # cycle on all nodes
+    for j in xrange(n):                         # compare each node with all the others
+        x = positions[i, 0] - positions[j, 0]   # compute x distance between node i and node j
+        y = positions[i, 1] - positions[j, 1]   # compute y distance between node i and node j
+        dist2 = x * x + y * y                   # compute distance square, avoid comp. of sqrt for comp. optim. reasons
+        if dist2 <= dmax2:                      # check on distance square
+            if dist2 != 0:                       # avoid considering self node as neighbor
+                node_list[i].neighbor_write(node_list[j])   # append operation on node's neighbor list
 
-for i in xrange(n):      # printing the results
-    #node_list[i].spec()
-    [x[i], y[i]] = node_list[i].get_pos()
-for i in sensors_position:
-    [u[i], v[i]] = node_list[i].get_pos()
-
-elapsed = time.time() - t
+elapsed = time.time() - t                       # computation of elapsed time
 print elapsed
 
 plt.title("Graphical representation of sensors' positions")
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.grid()
-plt.xticks([5*k for k in xrange(L/5+1)])
-plt.yticks([5*k for k in xrange(L/5+1)])
-plt.axis([-1, L+1, -1, L+1])
-plt.plot(x, y, linestyle='', marker='o')
-plt.plot(u, v, color='red', linestyle='', marker='o')
+#plt.grid()
+plt.xticks([5 * k for k in xrange(L / 5 + 1)])
+plt.yticks([5 * k for k in xrange(L / 5 + 1)])
+plt.axis([-1, L + 1, -1, L + 1])
+plt.plot(positions[:, 0], positions[:, 1], linestyle='', marker='o', label='Storage')
+plt.plot(positions[sensors_position, 0], positions[sensors_position, 1], color='red', linestyle='', marker='o' ,label='Sensor')
+plt.legend(loc='upper left')
 plt.show()
-
