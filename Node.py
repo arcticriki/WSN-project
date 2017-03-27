304202d9993c1a460b0a1b904823fdee0001e047
arcticriki <riccardo.belluzzo@gmail.com>
2017-03-27 23:37:12 +0200

    My first commitment

--- Node.py
+++ Node.py
@@ -11,14 +11,14 @@ class Storage(object):
         self.ID = ID                                # ID of the node
         self.X = X                                  # position
         self.Y = Y                                  # position
-        self.neighbor_list = []                     # list of references for neigbor nodes
+        self.neighbor_list = []                     # list of references for neighbor nodes
         self.node_degree = 0                        # number of neighbors
         self.out_buffer = []                        # outgoing packets buffer
         self.dim_buffer = 0                         # number of pkt in the outgoing queue
         self.code_degree = d                        # degree of the node, define how many pkts to encode
         self.ID_list =[]                            # ID list of encoded pkts, saved for decoding purposes
         self.storage = [np.random.randint(0, 1) for _ in xrange(payload)]  # initialization of storage variable [0,..,0]
-        self.num_encoded = 0                        # num. of encoded pkts, used to stop encoding process if it reaces d
+        self.num_encoded = 0                        # num. of encoded pkts, used to stop encoding process if it reaches d
 
         self.n = n                                  # number of nodes in the network
         self.k = k                                  # number of sensors in the network
@@ -43,12 +43,12 @@ class Storage(object):
         self.node_degree += 1                       # increase neighbor number at each insertion
 
     def send_pkt(self, mode):                       # send function used to move messages between nodes
-        if self.dim_buffer > 0:                     # if buffer non empy --> we can send something
+        if self.dim_buffer > 0:                     # if buffer non empty --> we can send something
             if mode == 0:                           # mode 0 = uniform at random selection
                 vicino = rnd.choice(self.neighbor_list)
                 pkt = self.out_buffer.pop(0)
                 self.dim_buffer -= 1
-                return vicino.receive_pkt(pkt)      # pass pkt to neighobor and return 1 if blocked or 0 if not blocked
+                return vicino.receive_pkt(pkt)      # pass pkt to neighbor and return 1 if blocked or 0 if not blocked
             elif mode == 1:                         # mode 1 = metropolis algo (ancora da implementare per paper 1)
                 print 'Metropolis algorithm not yet implemented'
         else:                   # empy buffer
@@ -57,8 +57,8 @@ class Storage(object):
 
     def receive_pkt(self, pkt):                 # define what to do on pkt receiving
         self.visits[pkt.ID-1] += 1              # increase number of visits this pkt has done in this very node
-        if self.visits[pkt.ID-1] > 1 and pkt.counter >= C1 * self.n * np.log(self.n):  # if packet already visited the node
-                                                # and its counter is greater than C1nlog(n) then, discard it
+        if self.visits[pkt.ID-1] > 1 and pkt.counter >= C1 * self.n * np.log10(self.n):  # if packet already visited the node
+                                                # and its counter is greater than C1nlog10(n) then, discard it
             return 1                            # pkt dropped
         else:
             if self.visits[pkt.ID - 1] == 1 and self.num_encoded < self.code_degree:
@@ -70,7 +70,7 @@ class Storage(object):
                     self.num_encoded += 1              # increase num of encoded pkts
 
             pkt.counter += 1                         # increase pkt counter then put it in the outgoing buffer
-            self.out_buffer.append(pkt)         # else, if pkt is at its first visit, or it haven't reached C1nlog(n)
+            self.out_buffer.append(pkt)         # else, if pkt is at its first visit, or it haven't reached C1nlog10(n)
             self.dim_buffer += 1
             return 0
 
