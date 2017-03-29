69047cd081269862db536ac7519e38da99ef25ea
Soldelli92 <mattia.soldan.ms@gmail.com>
2017-03-29 09:14:32 +0200

    FIXED DEGREE GENERATION

---
+++ RSD.py
@@ -0,0 +1,59 @@
+import numpy as np
+from scipy import stats
+
+# Robust Solition Distribution
+
+
+def robust_soliton_distribution(k, c0, delta, n):
+    R = c0 * np.sqrt(k) * np.log(float(k)/delta)
+    print k/R
+    d = np.zeros(n)
+
+    #Tau Function
+    tau = []
+    for i in range(1, k+1):
+        if i <= round((k/R) - 1):
+            tau.append(float(R)/(i*k))
+        elif i >= round((k/R)+1):
+            tau.append(0)
+        else: #i == (k/R):
+            tau.append((float(R)*np.log(float(R)/delta))/k)
+
+    # Ideal Soliton Distribution
+    sd = [1.0 / k]
+    [sd.append(1.0 / (j * (j - 1))) for j in range(2, k+1)]
+
+    # Beta
+    beta =np.sum([tau[i]+sd[i] for i in xrange(k)])
+
+    # Robust Soliton Distribution
+    pdf = []
+    for i in xrange(k):
+        pdf.append((tau[i] + sd[i])/beta)
+
+    # plt.subplot(3, 1, 1)
+    # plt.title('Tau')
+    # y = tau[0:50]
+    # x = np.linspace(1, 50, 50, endpoint=True)
+    # plt.bar(x, y)
+    # #plt.axis([0, 50, 0, 0.45])
+    # plt.subplot(3, 1, 2)                              PLOT CODE FOR TAU/SD/PDF VARIABLES, SUBPLOT TYPE
+    # plt.title('Robust soliton cdf')
+    # y1 = sd[0:50]
+    # plt.bar(x, y1, color='red')
+    # #plt.axis([0, 50, 0, 1])
+    # plt.subplot(3, 1, 3)
+    # plt.title('Robust soliton cdf')
+    # y2 = pdf[0:50]
+    # plt.bar(x, y2, color='red')
+    # #plt.axis([0, 50, 0, ])
+    # plt.show()
+
+    xk = np.arange(1, k+1)                                      # Prob[X=xk]=pk. Thus, xk={indices of vector pdf}
+    custm = stats.rv_discrete(name='custm', values=(xk, pdf))   # create object stats.custm that represents our distribution
+
+    for i in xrange(n):
+        d[i] = custm.rvs()                          # randomly sample an element from custm, following the
+                                                    # distribution of custm
+    return d
+
