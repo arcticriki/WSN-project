353dfed07ee825175a58cd82b9a9b2935460bf6e
Soldelli92 <mattia.soldan.ms@gmail.com>
2017-03-29 09:11:46 +0200

    FIXED DEGREE GENERATION

--- ProvaGio.py
+++ ProvaGio.py
@@ -4,8 +4,10 @@ import time as time
 from scipy import stats
 
 # Robust Solition Distribution
-def Robust_Soliton_Distribution(k, c0, delta):
+def Robust_Soliton_Distribution(k, c0, delta, n):
     R = c0 * np.sqrt(k) * np.log(float(k)/delta)
+    print k/R
+    d = np.zeros(n)
 
     #Tau Function
     tau = []
@@ -25,67 +27,39 @@ def Robust_Soliton_Distribution(k, c0, delta):
     beta =np.sum([tau[i]+sd[i] for i in xrange(k)])
 
     # Robust Soliton Distribution
-    rsd = []
+    pdf = []
     for i in xrange(k):
-        rsd.append((tau[i] + sd[i])/beta)
-    return rsd
+        pdf.append((tau[i] + sd[i])/beta)
 
+    # plt.subplot(3, 1, 1)
+    # plt.title('Tau')
+    # y = tau[0:50]
+    # x = np.linspace(1, 50, 50, endpoint=True)
+    # plt.bar(x, y)
+    # #plt.axis([0, 50, 0, 0.45])
+    # plt.subplot(3, 1, 2)                              PLOT CODE FOR TAU/SD/PDF VARIABLES, SUBPLOT TYPE
+    # plt.title('Robust soliton cdf')
+    # y1 = sd[0:50]
+    # plt.bar(x, y1, color='red')
+    # #plt.axis([0, 50, 0, 1])
+    # plt.subplot(3, 1, 3)
+    # plt.title('Robust soliton cdf')
+    # y2 = pdf[0:50]
+    # plt.bar(x, y2, color='red')
+    # #plt.axis([0, 50, 0, ])
+    # plt.show()
 
-k = 200
-pdf = Robust_Soliton_Distribution(k, c0=0.2, delta=0.05)
-#cdf = np.zeros(k)
-#cdf[0] = pdf[0]
-#for i in xrange(k-1):
-#    cdf[i+1] = cdf[i] + pdf[i+1]
+    xk = np.arange(1, k+1)                                      # Prob[X=xk]=pk. Thus, xk={indices of vector pdf}
+    custm = stats.rv_discrete(name='custm', values=(xk, pdf))   # create object stats.custm that represents our distribution
 
-t = time.time()
-d = np.zeros(k)
-
-kkk = 1000
-medio = np.zeros(kkk)
-
-for j in xrange(kkk):
-    for i in xrange(k):
-        d[i] = np.random.choice(np.arange(1, k+1), p=pdf)
-    medio[j] = float(np.sum(d))/k
-
-m = float(np.sum(medio))/len(medio)
-elapsed = time.time() - t
-print elapsed, m
-
-# ---------Distribution sampling Demo
-t = time.time()
-xk = np.arange(k)                                           #Prob[X=xk]=pk. Thus, xk={indices of vector pdf}
-pk = pdf                                                    #and pk={elements of vector pdf}
-custm = stats.rv_discrete(name='custm', values=(xk, pk))    #create object stats.custm that represents our distribution
-sample = np.zeros(k)
-
-for j in xrange(kkk):
-    for i in xrange(k):
-        sample[i] = custm.rvs()                             #randomly sample an element from custm, following the
+    for i in xrange(n):
+        d[i] = custm.rvs()                             #randomly sample an element from custm, following the
                                                             #distribution of custm
-    medio[j] = float(np.sum(sample))/k
-
-m = float(np.sum(medio))/len(medio)
-elapsed = time.time() - t
-print elapsed, m
+    return d
 
 
-
-
-# plt.subplot(2, 1, 1)
-# plt.title('Robust soliton pdf')
-# y = pdf[0:50]
-# x = np.linspace(1, 50, 50, endpoint=True)
-# plt.bar(x, y)
-# plt.axis([0, 50, 0, 0.45])
-# plt.subplot(2, 1, 2)
-# plt.title('Robust soliton cdf')
-# y1 = cdf[0:50]
-# plt.bar(x, y1, color='red')
-# plt.axis([0, 50, 0, 1])
-# plt.show()
-
+d = Robust_Soliton_Distribution(k=10000, c0=0.2, delta=0.05, n=100000)
+print d
 # plt.subplot(2, 1, 1)
 # plt.title('Robust soliton pdf')
 # y = pdf[0:50]
