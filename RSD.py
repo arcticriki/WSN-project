72137cade0a5120a619172e7f35dc9ad0a17196e
Soldelli92 <mattia.soldan.ms@gmail.com>
2017-05-22 23:26:34 +0200

    Implementation of paper 1

--- RSD.py
+++ RSD.py
@@ -40,31 +40,36 @@ def Robust_Soliton_Distribution(n, k, c0, delta):
     for i in xrange(k):
         pdf.append((tau[i] + sd[i])/beta)                   # computation of pdf using tau, sd and the normalization coeff
 
+
+
+    upper = 50
+    if len(pdf)<50:
+        upper = len(pdf)
     plt.subplot(3, 1, 1)
     plt.title('Tau')
-    y = np.zeros(50)
+    #y = np.zeros(50)
     #y[:len(tau)] = tau
-    y = tau[0:50]
-    x = np.linspace(1, 50, 50, endpoint=True)
+    y = tau[0:upper]
+    x = np.linspace(1, upper, upper, endpoint=True)
     #plt.plot(x,y)
     plt.bar(x, y)
-    plt.axis([0, 50, 0, 0.05])
+    plt.axis([0, upper, 0, np.max(tau)])
     plt.subplot(3, 1, 2)                            #PLOT CODE FOR TAU/SD/PDF VARIABLES, SUBPLOT TYPE
     plt.title('Robust soliton cdf')
-    y1 = np.zeros(50)
+    #y1 = np.zeros(50)
     #y1[:len(sd)] = sd
-    y1 = sd[0:50]
+    y1 = sd[0:upper]
     #plt.plot(x, y1, color='red')
     plt.bar(x, y1, color='red')
-    plt.axis([0, 50, 0, 0.6])
+    plt.axis([0, upper, 0, 0.6])
     plt.subplot(3, 1, 3)
     plt.title('Robust soliton cdf')
-    y2 = np.zeros(50)
+    #y2 = np.zeros(50)
     #y2[:len(pdf)] = pdf
-    y2 = pdf[0:50]
+    y2 = pdf[0:upper]
     #plt.plot(x, y2, color='red')
     plt.bar(x, y2, color='red')
-    plt.axis([0, 50, 0, 0.6])
+    plt.axis([0, upper, 0, 0.6])
     plt.show()
 
     xk = np.arange(1, k+1)                                  # vector of degree, start from 1 and goes up to k
